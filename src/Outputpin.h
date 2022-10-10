589f1bca013d4b4e3376bf9472bc06736a783c4a
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2022-10-10 10:05:43 -0500

    Revisión de bug 10

--- src/Outputpin.h
+++ src/Outputpin.h
@@ -3,5 +3,42 @@
 
 #include <Arduino.h>
 
+namespace edwinspire
+{
+
+    enum BlinkState
+    {
+        DISABLE = 0,
+        DELAY = 1,
+        BLINK = 2
+    };
+
+    class OutputPin
+    {
+
+    public:
+        void setup(int pin);
+        void high(void);
+        void low(void);
+        void blink(unsigned long lowTime, unsigned long highTime, unsigned long delayTime, long blinkTimes);
+        void blink(unsigned long lowTime, unsigned long highTime, unsigned long delayTime);
+        void blink(unsigned long lowTime, unsigned long highTime);
+        int getState(void);
+
+        void loop(void);
+
+    private:
+        int _outputPin;
+        int _outputState;
+        BlinkState _blinkState;
+
+        unsigned long _highTime;
+        unsigned long _lowTime;
+        unsigned long _startTime;
+        unsigned long _blinkTimes;
+        unsigned long _lastBlinkTime; // the last time the output pin was blinked
+    };
+
+}
 
 #endif
